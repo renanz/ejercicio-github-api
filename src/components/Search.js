6daf91011875cbc18bf8cae41c3048df5da98c10
renanz <renan_zelaya@hotmail.com>
2018-10-31 23:54:07 -0600

    search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import {Form, Button, FormGroup, FormControl } from 'react-bootstrap';
 
-const Search = () => {
+const Search = ({ onSubmitUsername }) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    if (onSubmitUsername && username) {
+      onSubmitUsername(username);
+    }
+  };
   return (
     <Form inline>
       <FormGroup controlId="formInlineEmail">
-        <FormControl type="text" placeholder="Enter username" />
+        <FormControl type="text" placeholder="Enter username" name="username" onSubmit={handleSubmit} />
       </FormGroup>{' '}
-      <Button type="submit">Add</Button>
+      <Button type="submit" >Add</Button>
     </Form>
-  )
-}
+  );
+};
+
+Search.propTypes = {
+  onSubmitUsername: PropTypes.func
+};
 
 export default Search;
